e4b5e3f28ddc5ae8c07a4fd47d6098a70a49571f
InterwebXplorer <InterwebXplorer@gmail.com>
2021-09-27 09:07:16 +1300

    Path update

--- Library/Agentpaths/astrapaths.py
+++ Library/Agentpaths/astrapaths.py
@@ -1,5 +1,11 @@
 ASTRAPATHS = {
-    "ASTRAMODEL": [
-        ""
-    ]
+    "ASTRAFP": [
+        "FP_Rift_S0_Skelmesh.uasset" #First person
+    ],
+    "ASTRATP": [
+        "TP_Rift_S0_Skelmesh.uasset", #Third person
+    ],
+    "ASTRACS": [
+        "CS_Rift_S0_Skelmesh.uasset" #Character Select
+    ],
 }
