fabb808fec1e3f71e102d3330247f77b7dc359be
InterwebXplorer <InterwebXplorer@gmail.com>
2021-12-15 15:10:40 +1300

    Fixed typos and realized the node was for Body not Hair

--- Blender_Main.py
+++ Blender_Main.py
@@ -2,6 +2,7 @@ import bpy
 import ConfigParser
 import logging
 import os
+import json
 
 #Read Settings.ini
 config = ConfigParser()
@@ -29,27 +30,29 @@ YELLOW_RGB          = (255, 255, 0, 1)
 bpy.context.scene.use_nodes = True
 
 #Create node groups
-#def create_char_shader_node(context, operator, group_name)
+#def create_hair_shader_node(context, operator, group_name)
 
-def create_hair_shader_node(context, operator, group_name) :
-    hair_shader_node = bpy.data.node_groups.new(group_name, "CompositorNodeTree")
+#Body Shader Node (Not sure if needed)
+def create_body_shader_node(context, operator, group_name) :
+    body_shader_node = bpy.data.node_groups.new(group_name, "CompositorNodeTree")
     #Group Input
-    group_in = hair_shader_node.nodes.new("NodeGroupInput")
-    hair_shader_node.inputs.new("NodeSocketImage","Diffuse")
-    hair_shader_node.inputs.new("NodeSocketImage","MRS")
-    hair_shader_node.inputs.new("NodeSocketImage","Normal")
+    group_in = body_shader_node.nodes.new("NodeGroupInput")
+    body_shader_node.inputs.new("NodeSocketImage","Diffuse")
+    body_shader_node.inputs.new("NodeSocketImage","MRS")
+    body_shader_node.inputs.new("NodeSocketImage","Normal")
     #Group Output
-    group_out = hair_shader_node.nodes.new("NodeGroupOutput")
-    hair_shader_node.Outputs.new("NodeSocketColor","Output")
+    group_out = body_shader_node.nodes.new("NodeGroupOutput")
+    body_shader_node.Outputs.new("NodeSocketColor","Output")
     #Nodes
-    seperate_rgb    = hair_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
-    seperate_rgb    = hair_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
-    invert          = hair_shader_node.nodes.new(type= "CompositorNodeInvert")
-    combine_rgb     = hair_shader_node.nodes.new(type= "CompositorNodeCombineRGB")
-    normal_map      = hair_shader_node.nodes.new(type= "CompositorNodeNormalMap")
-    principled_bdsf = hair_shader_node.nodes.new(type= "CompositorNodePrincipledBDSF")
+    seperate_rgb    = body_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
+    seperate_rgb    = body_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
+    invert          = body_shader_node.nodes.new(type= "CompositorNodeInvert")
+    combine_rgb     = body_shader_node.nodes.new(type= "CompositorNodeCombineRGB")
+    normal_map      = body_shader_node.nodes.new(type= "CompositorNodeNormalMap")
+    principled_bdsf = body_shader_node.nodes.new(type= "CompositorNodePrincipledBDSF")
+    shader_to_rgb   = body_shader_node.nodes.new(type= "CompositorNodeShaderToRGB")         #Due to this only eevee is supported
     #Connections
-    Link = hair_shader_node.links.new
+    Link = body_shader_node.links.new
     Link(group_in.outputs[0], principled_bdsf.inputs[1])
     Link(group_in.outputs[1], seperate_rgb.inputs[0])
     Link(seperate_rgb.outputs[0], principled_bdsf.inputs[4])
@@ -61,11 +64,12 @@ def create_hair_shader_node(context, operator, group_name) :
     Link(seperate_rgb.outputs[2], combine_rgb.inputs[2])
     Link(combine_rgb.outputs[0], normal_map.inputs[1])
     Link(normal_map.outputs[0], principled_bdsf.inputs[20])
-    Link(principled_bdsf.outputs[0], ggroup_out.inputs[0])
-    return hair_shader_node
-    custom_node_name = "Hair Shader"
-    my_group = create_hair_shader_node(self, context, custom_node_name)
-    hair_shader_node = context.scene.node_tree.nodes.new("CompositorNodeGroup")
-    hair_shader_node.node_tree = bpy.data.node_groups[my_group.name]
+    Link(principled_bdsf.outputs[0], shader_to_rgb.inputs[0])
+    Link(shader_to_rgb.outputs[0], group_out.inputs[0])
+    return body_shader_node
+custom_node_name = "Body Shader"
+my_group = create_body_shader_node(self, context, custom_node_name)
+body_shader_node = context.scene.node_tree.nodes.new("CompositorNodeGroup")
+body_shader_node.node_tree = bpy.data.node_groups[my_group.name]
 
 #def create_eye_shader_node(context, operator, group_name)
