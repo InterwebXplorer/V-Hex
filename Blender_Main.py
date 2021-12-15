af7c5e3507830c88da00909813a461166a74e074
InterwebXplorer <InterwebXplorer@gmail.com>
2021-12-15 21:28:41 +1300

    Progress on node generation

--- Blender_Main.py
+++ Blender_Main.py
@@ -2,7 +2,7 @@ import bpy
 import ConfigParser
 import logging
 import os
-import json
+import json #I pray to god that I wont have to use this
 
 #Read Settings.ini
 config = ConfigParser()
@@ -29,10 +29,22 @@ YELLOW_RGB          = (255, 255, 0, 1)
 #Enable nodes
 bpy.context.scene.use_nodes = True
 
-#Create node groups
-#def create_hair_shader_node(context, operator, group_name)
+#Create node groups @Note: Due to the use of the Shader To RGB Node eevee is only supported 
+#Hair Shader Node
+def create_hair_shader_node(context, operator, group_name) :
+    hair_shader_node = bpy.data.node_groups.new(group_name, "CompositorNodeTree")
+    #Group Input
+    group_in = hair_shader_node.nodes.new("NodeGroupInput")
+    hair_shader_node.inputs.new("NodeSocketImage","Diffuse")
+    hair_shader_node.inputs.new("NodeSocketImage","MRS")
+    hair_shader_node.inputs.new("NodeSocketImage","Normal")
+    #Group Output
+    group_out = hair_shader_node.nodes.new("NodeGroupOutput")
+    hair_shader_node.Outputs.new("NodeSocketColor","Output")
+    #Nodes THIS ONE IS CONFUSING AF
 
-#Body Shader Node (Not sure if needed)
+
+#Body Shader Node
 def create_body_shader_node(context, operator, group_name) :
     body_shader_node = bpy.data.node_groups.new(group_name, "CompositorNodeTree")
     #Group Input
@@ -45,12 +57,12 @@ def create_body_shader_node(context, operator, group_name) :
     body_shader_node.Outputs.new("NodeSocketColor","Output")
     #Nodes
     seperate_rgb    = body_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
-    seperate_rgb    = body_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
+    seperate_rgb2   = body_shader_node.nodes.new(type= "CompositorNodeSeperateRGB")
     invert          = body_shader_node.nodes.new(type= "CompositorNodeInvert")
     combine_rgb     = body_shader_node.nodes.new(type= "CompositorNodeCombineRGB")
     normal_map      = body_shader_node.nodes.new(type= "CompositorNodeNormalMap")
     principled_bdsf = body_shader_node.nodes.new(type= "CompositorNodePrincipledBDSF")
-    shader_to_rgb   = body_shader_node.nodes.new(type= "CompositorNodeShaderToRGB")         #Due to this only eevee is supported
+    shader_to_rgb   = body_shader_node.nodes.new(type= "CompositorNodeShaderToRGB")
     #Connections
     Link = body_shader_node.links.new
     Link(group_in.outputs[0], principled_bdsf.inputs[1])
@@ -58,10 +70,10 @@ def create_body_shader_node(context, operator, group_name) :
     Link(seperate_rgb.outputs[0], principled_bdsf.inputs[4])
     Link(seperate_rgb.outputs[1], principled_bdsf.inputs[5])
     Link(seperate_rgb.outputs[2], principled_bdsf.inputs[7])
-    Link(group_in.outputs[2], seperate_rgb.inputs[0])
-    Link(seperate_rgb.outputs[0], combine_rgb.inputs[0])
-    Link(seperate_rgb.outputs[1], invert.inputs[1])
-    Link(seperate_rgb.outputs[2], combine_rgb.inputs[2])
+    Link(group_in.outputs[2], seperate_rgb2.inputs[0])
+    Link(seperate_rgb2.outputs[0], combine_rgb.inputs[0])
+    Link(seperate_rgb2.outputs[1], invert.inputs[1])
+    Link(seperate_rgb2.outputs[2], combine_rgb.inputs[2])
     Link(combine_rgb.outputs[0], normal_map.inputs[1])
     Link(normal_map.outputs[0], principled_bdsf.inputs[20])
     Link(principled_bdsf.outputs[0], shader_to_rgb.inputs[0])
@@ -72,4 +84,33 @@ my_group = create_body_shader_node(self, context, custom_node_name)
 body_shader_node = context.scene.node_tree.nodes.new("CompositorNodeGroup")
 body_shader_node.node_tree = bpy.data.node_groups[my_group.name]
 
-#def create_eye_shader_node(context, operator, group_name)
+#Eye Shader Node
+def create_eye_shader_node(context, operator, group_name) :
+    eye_shader_node = bpy.data.node_groups.new(group_name, "CompositorNodeTree")
+    #Group Input
+    group_in = eye_shader_node.nodes.new("NodeGroupInput")
+    eye_shader_node.inputs.new("NodeSocketImage","Diffuse")
+    eye_shader_node.inputs.new("NodeSocketFactor","Alpha")
+    #Group Output
+    group_out = eye_shader_node.nodes.new("NodeGroupOutput")
+    eye_shader_node.Outputs.new("NodeSocketColor","Output")
+    #Nodes
+    diffuse_bdsf      = eye_shader_node.nodes.new(type= "CompositorNodeDiffuseBDSF")
+    transparent_bdsf  = eye_shader_node.nodes.new(type= "CompositorNodeTransparentBDSF")
+    mix_shader        = eye_shader_node.nodes.new(type= "CompositorNodeMixShader")
+    shader_to_rgb     = eye_shader_node.nodes.new(type= "CompositorNodeShaderToRGB")
+    #Connections
+    Link = eye_shader_node.links.new
+    Link(group_in.outputs[0], diffuse_bdsf.inputs[0])
+    Link(group_in.outputs[1], mix_shader.inputs[0])
+    Link(transparent_bdsf.outputs[1], mix_shader.inputs[1])
+    Link(diffuse_bdsf.outputs[0], mix_shader.inputs[2])
+    Link(mix_shader.outputs[0], shader_to_rgb.inputs[0])
+    Link(shader_to_rgb.outputs[0], group_out.inputs[0])
+    return eye_shader_node
+custom_node_name = "Eye Shader"
+my_group = create_eye_shader_node(self, context, custom_node_name)
+eye_shader_node = context.scene.node_tree.nodes.new("CompositorNodeGroup")
+eye_shader_node.node_tree = bpy.data.node_groups[my_group.name]
+
+#def create_vfx_shader_node(context, operator, group_name) :
