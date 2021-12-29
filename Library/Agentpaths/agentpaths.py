7f58901a71c4d0eda9aa9cd9bcf28a8e7391bbd0
InterwebXplorer <InterwebXplorer@gmail.com>
2021-12-29 23:24:46 +1300

    Merged agent paths

---
+++ Library/Agentpaths/agentpaths.py
@@ -0,0 +1,238 @@
+ASTRAPATHS = {
+    "ASTRAFP": [
+        "FP_Rift_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Rift/S0/1P/Models/FP_Rift_S0_Skelmesh.uasset
+    ],
+    "ASTRATP": [
+        "TP_Rift_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Rift/S0/3P/Models/TP_Rift_S0_Skelmesh.uasset
+    ],
+    "ASTRACS": [
+        "CS_Rift_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Rift/S0/CharSelect/Models/CS_Rift_S0_Skelmesh.uasset
+    ],
+}
+BREACHPATHS = {
+    "BREACHFP": [
+        "FP_Breach_New_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Breach/S0/1P/Models/FP_Breach_New_S0_Skelmesh.uasset
+    ],
+    "BREACHTP": [
+        "TP_Breach_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Breach/S0/3P/Models/TP_Breach_S0_Skelmesh.uasset
+    ],
+    "BREACHCS": [
+        "CS_Breach_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Breach/S0/CharSelect/Models/CS_Breach_S0_Skelmesh.uasset
+    ],
+}
+BRIMSTONEPATHS = {
+    "BRIMSTONEFP": [
+        "FP_Sarge_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Sarge/S0/1P/Models/FP_Sarge_S0_Skelmesh.uasset
+    ],
+    "BRIMSTONETP": [
+        "TP_Sarge_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Sarge/S0/3P/Models/TP_Sarge_S0_Skelmesh.uasset
+    ],
+    "BRIMSTONECS": [
+        "CS_Sarge_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Sarge/S0/CharSelect/Models/CS_Sarge_S0_Skelmesh.uasset
+    ],
+}
+CHAMBERPATHS = {
+    "CHAMBERFP": [
+        "" #First person
+        #
+    ],
+    "CHAMBERTP": [
+        "", #Third person
+        #
+    ],
+    "CHAMBERCS": [
+        "" #Character Select
+        #
+    ],
+}
+CYPHERPATHS = {
+    "CYPHERFP": [
+        "FP_Gumshoe_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Gumshoe/S0/1P/Models/FP_Gumshoe_S0_Skelmesh.uasset
+    ],
+    "CYPHERTP": [
+        "TP_Gumshoe_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Gumshoe/S0/3P/Models/TP_Gumshoe_S0_Skelmesh.uasset
+    ],
+    "CYPHERCS": [
+        "CS_Gumshoe_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Gumshoe/S0/CharSelect/Models/CS_Gumshoe_S0_Skelmesh.uasset
+    ],
+}
+JETTPATHS = {
+    "JETTFP": [
+        "FP_Wushu_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Wushu/S0/1P/Models/FP_Wushu_S0_Skelmesh.uasset
+    ],
+    "JETTTP": [
+        "TP_Wushu_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Wushu/S0/3P/Models/TP_Wushu_S0_Skelmesh.uasset
+    ],
+    "JETTCS": [
+        "CS_Wushu_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Wushu/S0/CharSelect/Models/CS_Wushu_S0_Skelmesh.uasset
+    ],
+}
+KAYOPATHS = {
+    "KAYOFP": [
+        "FP_Grenadier_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Grenadier/S0/1P/Models/FP_Grenadier_S0_Skelmesh.uasset
+    ],
+    "KAYOTP": [
+        "TP_Grenadier_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Grenadier/S0/3P/Models/TP_Grenadier_S0_Skelmesh.uasset
+    ],
+    "KAYOCS": [
+        "CS_Grenadier_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Grenadier/S0/CharSelect/Models/CS_Grenadier_S0_Skelmesh.uasset
+    ],
+}
+KILLJOYPATHS = {
+    "KILLJOYFP": [
+        "FP_Killjoy_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Killjoy/S0/1P/Models/FP_Killjoy_S0_Skelmesh.uasset
+    ],
+    "KILLJOYTP": [
+        "TP_Killjoy_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Killjoy/S0/3P/Models/TP_Killjoy_S0_Skelmesh.uasset
+    ],
+    "KILLJOYCS": [
+        "CS_Killjoy_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Killjoy/S0/CharSelect/Models/CS_Killjoy_S0_Skelmesh.uasset
+    ],
+}
+OMENPATHS = {
+    "OMENFP": [
+        "FP_Wraith_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Wraith/S0/1P/Models/FP_Wraith_S0_Skelmesh.uasset
+    ],
+    "OMENTP": [
+        "TP_Wraith_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Wraith/S0/3P/Models/TP_Wraith_S0_Skelmesh.uasset
+    ],
+    "OMENCS": [
+        "CS_Wraith_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Wraith/S0/CharSelect/Models/CS_Wraith_S0_Skelmesh.uasset
+    ],
+}
+PHEONIXPATHS = {
+    "PHEONIXFP": [
+        "FP_Phoenix_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Phoenix/S0/1P/Models/FP_Phoenix_S0_Skelmesh.uasset
+    ],
+    "PHEONIXTP": [
+        "TP_Phoenix_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Phoenix/S0/3P/Models/TP_Phoenix_S0_Skelmesh.uasset
+    ],
+    "PHEONIXCS": [
+        "CS_Phoenix_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Phoenix/S0/CharSelect/Models/CS_Phoenix_S0_Skelmesh.uasset
+    ],
+}
+RAZEPATHS = {
+    "RAZEFP": [
+        "FP_Clay_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Clay/S0/1P/Models/FP_Clay_S0_Skelmesh.uasset
+    ],
+    "RAZETP": [
+        "TP_Clay_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Clay/S0/3P/Models/TP_Clay_S0_Skelmesh.uasset
+    ],
+    "RAZECS": [
+        "CS_Clay_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Clay/S0/CharSelect/Models/CS_Clay_S0_Skelmesh.uasset
+    ],
+}
+REYNAPATHS = {
+    "REYNAFP": [
+        "FP_Vampire_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Vampire/S0/1P/Models/FP_Vampire_S0_Skelmesh.uasset
+    ],
+    "REYNATP": [
+        "TP_Vampire_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Vampire/S0/3P/Models/TP_Vampire_S0_Skelmesh.uasset
+    ],
+    "REYNACS": [
+        "CS_Vampire_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Vampire/S0/CharSelect/Models/CS_Vampire_S0_Skelmesh.uasset
+    ],
+}
+SAGEPATHS = {
+    "SAGEFP": [
+        "FP_Thorne_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Thorne/S0/1P/Models/FP_Thorne_S0_Skelmesh.uasset
+    ],
+    "SAGETP": [
+        "TP_Thorne_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Thorne/S0/3P/Models/TP_Thorne_S0_Skelmesh.uasset
+    ],
+    "SAGECS": [
+        "CS_Thorne_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Thorne/S0/CharSelect/Models/CS_Thorne_S0_Skelmesh.uasset
+    ],
+}
+SKYEPATHS = {
+    "SKYEFP": [
+        "FP_Guide_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Guide/S0/1P/Models/FP_Guide_S0_Skelmesh.uasset
+    ],
+    "SKYETP": [
+        "TP_Guide_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Guide/S0/3P/Models/TP_Guide_S0_Skelmesh.uasset
+    ],
+    "SKYECS": [
+        "CS_Guide_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Guide/S0/CharSelect/Models/CS_Guide_S0_Skelmesh.uasset
+    ],
+}
+SOVAPATHS = {
+    "SOVAFP": [
+        "FP_Hunter_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Hunter/S0/1P/Models/FP_Hunter_S0_Skelmesh.uasset
+    ],
+    "SOVATP": [
+        "TP_Hunter_New_S0_Skelmesh1.uasset", #Third person
+        #/Game/Characters/Hunter/S0/3P/Models/TP_Hunter_New_S0_Skelmesh1.uasset
+    ],
+    "SOVACS": [
+        "CS_Hunter_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Hunter/S0/CharSelect/Models/CS_Hunter_S0_Skelmesh.uasset
+    ],
+}
+VIPERPATHS = {
+    "VIPERFP": [
+        "FP_Pandemic_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Pandemic/S0/1P/Models/FP_Pandemic_S0_Skelmesh.uasset
+    ],
+    "VIPERTP": [
+        "TP_Pandemic_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Pandemic/S0/3P/Models/TP_Pandemic_S0_Skelmesh.uasset
+    ],
+    "VIPERCS": [
+        "CS_Pandemic_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Pandemic/S0/CharSelect/Models/CS_Pandemic_S0_Skelmesh.uasset
+    ],
+}
+YORUPATHS = {
+    "YORUFP": [
+        "FP_Stealth_S0_Skelmesh.uasset" #First person
+        #/Game/Characters/Stealth/S0/1P/Models/FP_Stealth_S0_Skelmesh.uasset
+    ],
+    "YORUTP": [
+        "TP_Stealth_S0_Skelmesh.uasset", #Third person
+        #/Game/Characters/Stealth/S0/3P/Models/TP_Stealth_S0_Skelmesh.uasset
+    ],
+    "YORUCS": [
+        "CS_Stealth_S0_Skelmesh.uasset" #Character Select
+        #/Game/Characters/Stealth/S0/CharSelect/Models/CS_Stealth_S0_Skelmesh.uasset
+    ],
+}
